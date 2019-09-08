9625e0317357653db418586ddc3f61d1cbd29c9d
Akinnikemichael <52992676+Akinnikemichael@users.noreply.github.com>
2019-09-08 15:45:48 +0100

    Php
    checking out different around php codes

--- welcome_get.php
+++ welcome_get.php
@@ -1,13 +1,22 @@
+//<?php
+//error_reporting(E_ERROR|E_PARSE);
+//$name=$_POST["name"];
+//$email=$_POST["email"];
+//$PhoneNumber=$_POST["PhoneNumber"];
+//$Message=$_POST["Message"];
+//?>
 
-<!DOCTYPE html>
-<html>
-<body>
+//<?php
 
+//$file=fopen("text.txt","a");
+//fwrite($file,$name);
+//fwrite($file,$email);
+//fwrite($file,$PhoneNumber);
+//fwrite($file,$Message);
+//fclose($file);
+//?>
 
-
-
-Welcome <?php echo $_GET["nm"]; ?><br>
-Your email address is: <?php echo $_GET["email"]; ?>
-
-</body>
-</html>
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["email"]; ?><br>
+Your Phone Number is: <?php echo $_GET["PhoneNumber"]; ?>
+Your Message for Michael is: <?php echo $_GET["Message"]; ?>
